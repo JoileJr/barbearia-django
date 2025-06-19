18d9a873e359568edb68708a8bf6496ac631e3e6
JoileJr <joilejunior2@gmail.com>
2025-06-19 16:36:02 -0300

    templates

--- barbearia/urls.py
+++ barbearia/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from barbearia.views import my_view, sobre
+from barbearia.views import my_view, sobre, home
 
 urlpatterns = [
     path('my-view/', my_view),
     path('sobre/', sobre),
+    path('home/', home)
 ]
--- barbearia/views.py
+++ barbearia/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def my_view(request):
     return HttpResponse("Hello, world! This is my view.")
 
 def sobre(request):
-    return HttpResponse("Sobre.")
+    return render(request, 'barbearia/sobre.html')
+
+def home(request):
+    return render(request, 'home/home.html')
